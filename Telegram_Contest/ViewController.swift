c7c4235bc9fd7d1f121cc07370e753e74e410d12
Sad Tyoma <artyomshuneyko@gmail.com>
2022-10-13 13:18:09 +0300

    Initial Commit

---
+++ Telegram_Contest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telegram_Contest
+//
+//  Created by Artem Shuneyko on 13.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
